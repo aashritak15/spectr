065ee2f328d1843b448d08d240c7771716468f84
Aashrita Koyyalamudi <aashrita.koyyalamudi@gmail.com>
2024-06-25 14:19:57 -0700

    pswd manager

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+
 import Link from 'next/link';
 import React from 'react';
 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+
 const NavBar: React.FC = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const router = useRouter();
+
+    useEffect (() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser && JSON.parse(storedUser).loggedIn) {
+            setLoggedIn(true);
+        }
+
+    }, []);
+
     return (
         <nav className='bg-gray-800 p-4 justify-between items-center'>
 
@@ -15,10 +33,17 @@ const NavBar: React.FC = () => {
         
             <div className = "mr-20">
 
-                <Link href = "/profile">
-                        <span className = "bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">Profile</span>
-                
-                </Link>
+                {loggedIn ? ( 
+                    <Link href="/">
+                        <a className = "px-4 py-2 ml-4 text-sm font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600"> Profile </a>
+                    </Link>
+                    
+                ) : (
+                    <Link href="/login">
+                        <a className="px-4 py-2 ml-4 text-sm font-semibold text-white bg-green-500 rounded-md hover:bg-green-600"> Login </a>
+                    </Link>
+
+                )}
 
             </div>
         
--- src/app/pages/profile.tsx
+++ src/app/pages/profile.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+"use client";
+
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 const Profile = () => {
     const [user, setUser] = useState <{ username: string} | null>(null);
     const router = useRouter();
 
-    useEffect( () => {
+    useEffect(() => {
         const storedUser = localStorage.getItem('user');
         if(storedUser) {
             const userData = JSON.parse(storedUser);
@@ -29,9 +31,20 @@ const Profile = () => {
         router.push('/login');
     };
 
+    if(!user) return null; 
+
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            
+            <div className= "w-full max-w-md p-8 space-y-6 bg-white rounded-md shadow-md">
+                <h2 className="text-2xl font-bold text-center"> Profile Page </h2>
+                <p> Welcome, {user.username} </p>
+
+                <button onClick= {logoutProcess} className="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-md hover:bg-red-600">
+                    Logout
+                </button>
+            </div>
         </div>
-    )
+    );
 }
+
+export default Profile; 
